2a7d399e3c1abc0e78d5ad9186e45d26de80a425
Eduard Vartazaryan <e.vartazaryan@trueengineering.ru>
2025-09-14 13:03:37 +0700

    Initial Commit

---
+++ eventhub-ios-client/eventhub_ios_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventhub_ios_clientApp.swift
+//  eventhub-ios-client
+//
+//  Created by Эдуард Вартазарян on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct eventhub_ios_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eventhub-ios-clientTests/eventhub_ios_clientTests.swift
@@ -0,0 +1,17 @@
+//
+//  eventhub_ios_clientTests.swift
+//  eventhub-ios-clientTests
+//
+//  Created by Эдуард Вартазарян on 14.09.2025.
+//
+
+import Testing
+@testable import eventhub_ios_client
+
+struct eventhub_ios_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
